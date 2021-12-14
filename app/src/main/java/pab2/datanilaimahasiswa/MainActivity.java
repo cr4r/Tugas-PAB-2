e11e0800f1407b43a94f90c67414e1d5ffd0b1d0
fajrim081 <fajrim081@mhs.mdp.ac.id>
2021-12-14 13:40:16 +0700

    update first

---
+++ app/src/main/java/pab2/datanilaimahasiswa/MainActivity.java
@@ -0,0 +1,14 @@
+package pab2.datanilaimahasiswa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
